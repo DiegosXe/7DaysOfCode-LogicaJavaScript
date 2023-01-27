186f8f4c2d24febbbeb52ef6d4cea3059818d557
Diego Borelli <diegosxe18@gmail.com>
2023-01-26 21:25:46 -0300

    Dia 1 concluído

--- Day 1/Day 1.js
+++ Day 1/Day 1.js
@@ -5,45 +5,51 @@ let stringTrinta = "30";
 let numeroDez = 10;
 let stringDez = "10";
 
-document.write("numeroUm = 1 <br>");
-document.write("stringUm = '1' <br>");
+document.write("• numeroUm = 1 <br>");
+document.write("• stringUm = '1' <br>");
+
+document.write("<br>");
 
 if (numeroUm == stringUm) {
   document.write(
-    "As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes. <br>"
+    "⮚ As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes. <br>"
   );
 } else {
   document.write(
-    "As variáveis numeroUm e stringUm não tem o mesmo valor, <br>"
+    "⮚ As variáveis numeroUm e stringUm não tem o mesmo valor, <br>"
   );
 }
 
 document.write("<br><br>");
 
-document.write("numeroTrinta = 30 <br>");
-document.write("stringTrinta = '30' <br>");
+document.write("• numeroTrinta = 30 <br>");
+document.write("• stringTrinta = '30' <br>");
+
+document.write("<br>");
 
 if (numeroTrinta === stringTrinta) {
   document.write(
-    "As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo. <br>"
+    "⮚ As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo. <br>"
   );
 } else {
   document.write(
-    "As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo. <br>"
+    "⮚ As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo. <br>"
   );
 }
 
 document.write("<br><br>");
 
-document.write("numeroDez = 10 <br>");
-document.write("stringDez = '10' <br>");
+document.write("• numeroDez = 10 <br>");
+document.write("• stringDez = '10' <br>");
+
+document.write("<br>");
 
 if (numeroDez == stringDez) {
   document.write(
-    "As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes. <br>"
+    "⮚ As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes. <br>"
   );
 } else {
   document.write(
-    "As variáveis numeroDez e stringDez não tem o mesmo valor. <br>"
+    "⮚ As variáveis numeroDez e stringDez não tem o mesmo valor. <br>"
   );
 }
